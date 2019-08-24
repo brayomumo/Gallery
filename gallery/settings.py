6bb8e64839a092ef14444049c2898b3474fcc106
brayomumo <brayomumo5@gmail.com>
2019-08-24 23:49:38 +0300

    Register bootstrap and main as installed applications

--- gallery/settings.py
+++ gallery/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap3',
-    'news.apps.NewsConfig',
+    'main.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
