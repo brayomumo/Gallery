4ea72c3928634752f13d7173b0a574f05154f10c
brayomumo <brayomumo5@gmail.com>
2019-08-24 23:50:28 +0300

    Main config

--- gallery/settings.py
+++ gallery/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap3',
-    'main.apps.NewsConfig',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
