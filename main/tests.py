72b420f4524b3427fca3712f577ddf30eb6294b8
brayomumo <brayomumo5@gmail.com>
2019-08-25 01:21:53 +0300

    Test for the locations instance

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Location,categories,Image
+import datetime as dt
+# Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.kitui= Location(location='Nyeri')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kitui,Location))
